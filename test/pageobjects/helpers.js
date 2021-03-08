5e93c666a28f343978a6a347a0d31d74339e9abf
Abbas Aliyev <abbas@Abbass-MacBook-Pro.local>
2021-03-08 13:01:05 -0800

    update

--- test/pageobjects/helpers.js
+++ test/pageobjects/helpers.js
@@ -4,11 +4,9 @@ import Page from './page';
 class Helpers extends Page {
 
     get quoteInputField () { return $('._2Ff6O56evM.sc-bdVaJa.dmWroL>div>div>div>div>input')}
-    get quoteInfoButtonRHHBY () { return $('a[href^=\'/stock/RHHBY/quote\']') }
-    get quoteInfoButtonOTCM () { return $('a[href^=\'/stock/OTCM/quote\']') }
+    get quoteInfoButton () { return $('//a[text()="Quote"]') }
     get quoteMarketCap () { return $ ("//div[@class='_1G7n38q1bb sc-bdVaJa lbvrig'][11]/p")}
-    get securityDetailsInfoButtonRHHBY () { return $('a[href^=\'/stock/RHHBY/security\']') }
-    get securityDetailsInfoButtonOTCM () { return $('a[href^=\'/stock/OTCM/security\']') }
+    get securityDetailsInfoButton () { return $('//a[text()="Security Details"]') }
     get securityDetailsMarketCap () { return $$('._8AXJn4ourf.sc-htpNat.jtWIOA.sc-bdVaJa.gRrvFh>div.sc-bdVaJa.kYmYWE')[0]}
     get securityDetailsInfoDate () { return $$('._8AXJn4ourf.sc-htpNat.jtWIOA.sc-bdVaJa.gRrvFh>div.sc-bdVaJa.fKInnm')[0]}
 
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -5,44 +5,21 @@ describe('Scenarios', () => {
     let marketCapQuote;
     let marketCapSecurity;
     let marketCapDate;
-    it('scenario for RHHBY', () => {
-        browser.maximizeWindow();
-        Helpers.open();
-        Helpers.setCompanyName(Data.companies.company1);
-        browser.pause(2000);
-        browser.keys(['ArrowDown', 'Enter']);
-        Helpers.quoteInfoButtonRHHBY.click();
-        browser.pause(2000);
-        marketCapQuote = Helpers.quoteMarketCap.getText();
-        Helpers.securityDetailsInfoButtonRHHBY.click();
-        browser.pause(2000);
-        marketCapSecurity = Helpers.securityDetailsMarketCap.getText();
-        marketCapDate = Helpers.securityDetailsInfoDate.getText();
-
-    });
-    it('RHHBY Market caps equal', () => {
-        console.log(`Market Cap for RHHBY  ${marketCapSecurity} on ${marketCapDate}`)
-        expect(marketCapQuote).toEqual(marketCapSecurity);
-
-    });
-
-    it('scenario for OTCM', () => {
+    it(`scenario for ${Data.companies.company2}`, () => {
         browser.maximizeWindow();
         Helpers.open();
         Helpers.setCompanyName(Data.companies.company2);
         browser.pause(2000);
         browser.keys(['ArrowDown', 'Enter']);
-        Helpers.quoteInfoButtonOTCM.click();
-        browser.pause(2000);
+        Helpers.quoteInfoButton.click();
         marketCapQuote = Helpers.quoteMarketCap.getText();
-        Helpers.securityDetailsInfoButtonOTCM.click();
-        browser.pause(2000);
+        Helpers.securityDetailsInfoButton.click();
         marketCapSecurity = Helpers.securityDetailsMarketCap.getText();
         marketCapDate = Helpers.securityDetailsInfoDate.getText();
 
     });
-    it('OTCM Market caps equal', () => {
-        console.log(`Market Cap for OTCM ${marketCapSecurity} on ${marketCapDate}`)
+    it('Market caps equal', () => {
+        console.log(`Market Cap for ${Data.companies.company2}  ${marketCapSecurity} on ${marketCapDate}`)
         expect(marketCapQuote).toEqual(marketCapSecurity);
 
     });
